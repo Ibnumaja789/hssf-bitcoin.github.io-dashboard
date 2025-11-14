54d9bfb7f9959dabb70a9fe3d2ea40f87358b986
Ibnumaja789 <108216227+Ibnumaja789@users.noreply.github.com>
2025-11-14 23:01:47 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = 'hssf-v1';
+const FILES = ['/', '/index.html', '/manifest.json', '/assets/icon-192.png', '/assets/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
